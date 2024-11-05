920793db5c5892aedb6e46f641daf355b3b6516f
marques203 <felipe.sampaio.8795@ga.ita.br>
2024-11-05 16:28:47 -0300

    second commit

--- database/database.js
+++ database/database.js
@@ -1,8 +1,14 @@
 const Sequelize = require("sequelize");
+require('dotenv').config();
 
-const connection = new Sequelize('perguntas', 'root', '20314167',{
-    host: 'localhost',
-    dialect: 'mysql'
-});
+const connection = new Sequelize(
+    process.env.DB_NAME, 
+    process.env.DB_USER, 
+    process.env.DB_PASSWORD,
+    {
+        host: process.env.DB_HOST,
+        dialect: process.env.DB_DIALECT
+    }
+);
 
 module.exports = connection;
